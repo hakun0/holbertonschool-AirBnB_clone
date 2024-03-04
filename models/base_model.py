23cf1cbc95b9b4eef71a4c31211a19d1b8efdb6f
maelpseudo <mael.cuny@gmail.com>
2024-03-04 16:12:51 +0100

    Updated base_model.py and added base_model_test.py in /tests/

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+# models/base_model.py
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = dictionary['created_at'].isoformat()
+        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        return dictionary
